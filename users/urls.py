17511fe7b37b5aa5ca600ee2b30831f0ef7d6f8c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-23 19:29:07 +0530

    Completed module

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import  path
 from django.contrib.auth import  views as auth_views
+from . import  views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
